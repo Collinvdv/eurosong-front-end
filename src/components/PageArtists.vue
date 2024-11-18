2006bf72ce27995104e4fc44bc9affee754c8beb
Collin Van der Vorst <collinvandervorst@mac.lan>
2024-11-18 15:14:58 +0100

    Add artists page

--- src/components/PageArtists.vue
+++ src/components/PageArtists.vue
@@ -1,6 +1,22 @@
 <template>
     <div>
-        Page artists
+        <h1>
+            Artists
+        </h1>
+        
+        <ul>
+            <li v-for="(artist, index) in artists" :key="index">
+                {{ artist.name }}
+            </li>
+        </ul>
+
+        <hr>
+
+        <label> Artist </label>
+        <input type="text">
+        <button> 
+            Add artists
+        </button>
     </div>
 </template>
 
@@ -8,11 +24,24 @@
     export default {
         name: 'PageArtists',
         mounted() {
-            fetch("http://localhost:3000/api/artists")
-                .then(response => response.json())
-                .then(artists => {
-                    console.log(artists);
-                })
+            this.fetchArtists();
+        },
+        data() {
+            return {
+                artists: []
+            }
+        },
+        methods: {
+            fetchArtists() {
+                fetch("http://localhost:3000/api/artists")
+                    .then(response => response.json())
+                    .then(data => {
+                        this.artists = data;
+                    })
+            },
+            addArtist() {
+                console.log("Add artist");
+            }
         }
     }
 </script>
