330cf7a97bce3e9a42a351b76f3fc3cf6cc784f5
Collin Van der Vorst <collinvandervorst@DESKTOP-1OQ15O3.guests.thomasmore.be>
2024-11-21 17:16:56 +0100

    Voting

--- src/components/PageVoting.vue
+++ src/components/PageVoting.vue
@@ -5,23 +5,23 @@
         </h1>
         
         <div>
-            <button @click="prevSong()">
+            <button @click="prevSong()" :disabled="activeSongIndex == 0">
                 Prev song
             </button>
             
             <div v-if="songs.length > 0">
                 {{ songs[activeSongIndex].artist_name }} - Song {{ songs[activeSongIndex].song_name }}
             </div>
-            <button @click="nextSong()">
+            <button @click="nextSong()" :disabled="activeSongIndex == songs.length - 1">
                 Next song
             </button>
         </div>
 
         <hr>
         <div>
-            <button> Add 2 votes</button>
-            <button> Add 4 votes</button>
-            <button> Add 6 votes</button>
+            <button @click="addVote(2)" :disabled="this.votesSended.includes(2)"> Add 2 votes</button>
+            <button @click="addVote(4)" :disabled="this.votesSended.includes(4)"> Add 4 votes</button>
+            <button @click="addVote(6)" :disabled="this.votesSended.includes(6)"> Add 6 votes</button>
         </div>
     </div>
 </template>
@@ -35,10 +35,28 @@
         data() {
             return {
                 songs: [],
+                votesSended: [],
                 activeSongIndex: 0
             }
         },
         methods: {
+            addVote(points) {
+                fetch("http://localhost:3000/api/votes", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        song_id: this.songs[this.activeSongIndex].song_id,
+                        points: points
+                    })
+                })
+                    .then(response => response.json())
+                    .then((data) => {
+                        this.votesSended.push(points);
+                        console.log(data);
+                    })
+            },
             fetchSongs() {
                 fetch("http://localhost:3000/api/songs")
                     .then(response => response.json())
