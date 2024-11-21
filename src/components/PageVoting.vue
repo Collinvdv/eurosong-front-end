b963365bfc1eaf99dca1f861a2879e91d986edc5
Collin Van der Vorst <collinvandervorst@DESKTOP-1OQ15O3.guests.thomasmore.be>
2024-11-21 16:50:56 +0100

    Carousel

--- src/components/PageVoting.vue
+++ src/components/PageVoting.vue
@@ -9,18 +9,49 @@
                 Prev song
             </button>
             
-            <div>
-                CD - Song 1
+            <div v-if="songs.length > 0">
+                {{ songs[activeSongIndex].artist_name }} - Song {{ songs[activeSongIndex].song_name }}
             </div>
             <button>
                 Next song
             </button>
+        </div>
+
+        <hr>
+        <div>
+            <button> Add 2 votes</button>
+            <button> Add 4 votes</button>
+            <button> Add 6 votes</button>
         </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'PageVoting'
+        name: 'PageVoting',
+        mounted() {
+            this.fetchSongs();
+        },
+        data() {
+            return {
+                songs: [],
+                activeSongIndex: 0
+            }
+        },
+        methods: {
+            fetchSongs() {
+                fetch("http://localhost:3000/api/songs")
+                    .then(response => response.json())
+                    .then(data => {
+                        this.songs = data;
+                    })
+            },
+            prevSong() {
+                this.activeSongIndex--;
+            },
+            nextSong() {
+                this.activeSongIndex++;
+            },
+        }
     }
 </script>
